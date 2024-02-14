9884ea57555703dcf4e56961d5f970b5f3850b0f
tornado-13 <tornado-13@ADMIN-13s-iMac.local>
2024-02-14 17:33:10 +0800

    add

--- lesson_day_21/accounts/views.py
+++ lesson_day_21/accounts/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
+from .forms import CustomUserCreationFrom
 # Create your views here.
+
+class SignUpView():
+    form_class = CustomUserCreationFrom
+    success_url  = reverse_lazy("login")
+    template_name= "registration/signup.html"
