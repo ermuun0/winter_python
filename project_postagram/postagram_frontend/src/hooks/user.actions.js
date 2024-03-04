8811e23969c6fb14675163ad34dd56d17331d3b3
tornado-13 <tornado-13@ADMIN-13s-iMac.local>
2024-03-04 17:43:33 +0800

    ad

---
+++ project_postagram/postagram_frontend/src/hooks/user.actions.js
@@ -0,0 +1,57 @@
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
+function useUserActions() {
+  const navigate = useNavigate();
+  const baseURL = "http://localhost:8000/api";
+  return {
+    login,
+    register,
+    logout,
+  };
+  function login(data) {
+    return axios.post(`${baseURL}/auth/login/`, data).then((res) => {
+      // Registering the account and tokens in the
+      // store
+      setUserData(data);
+      navigate("/");
+    });
+  }
+  // Logout the user
+  function logout() {
+    localStorage.removeItem("auth");
+    navigate("/login");
+  }
+
+  
+}
+
+// Get the user
+function getUser() {
+  const auth = JSON.parse(localStorage.getItem("auth"));
+  return auth.user;
+}
+// Get the access token
+function getAccessToken() {
+  const auth = JSON.parse(localStorage.getItem("auth"));
+  return auth.access;
+}
+// Get the refresh token
+function getRefreshToken() {
+  const auth = JSON.parse(localStorage.getItem("auth"));
+  return auth.refresh;
+}
+// Set the access, token and user property
+function setUserData(data) {
+  localStorage.setItem(
+    "auth",
+    JSON.stringify({
+      access: data.access,
+      refresh: data.refresh,
+      user: data.user,
+    })
+  );
+  
+}
+
+export {useUserActions, getUser, getAccessToken, getRefreshToken}
---
+++ project_postagram/social-media-app/src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import ProtectedRoute from "./routes/ProtectedRoute";
+import Home from "./pages/Home";
+function App() {
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <ProtectedRoute>
+            <Home />
+          </ProtectedRoute>
+        }
+        
+      />
+      <Route path="/login/" element={<div>Login</div>} />
+      <Route path="/register/" element={<Registration />}
+      />
+      <Route path="/login/" element={<Login />} />
+    </Routes>
+  );
+}
+export default App;
